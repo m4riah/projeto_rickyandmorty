1c482afefce49998f80e8afba3195f79392e161c
m4riah <mariaeduardamend3s@gmail.com>
2023-06-22 13:00:50 -0300

    criando cards e animations

---
+++ js/script.js
@@ -0,0 +1,24 @@
+fetch(' https://rickandmortyapi.com/api/character', {
+    method: 'GET'
+})
+.then(response => response.json())
+.then(function(json){
+
+    var container = document.querySelector('.container');
+
+    json.results.map(function(results){
+
+        container.innerHTML+=`
+            <div class= "card"><img src= ` + results.image +`> 
+            <strong> ` + results.name + ` </strong><br>
+            <span> ` + results.species +` </span><br>
+            <p> ` + results.status + ` </p>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            </div>
+            `;
+    })
+    
+})
